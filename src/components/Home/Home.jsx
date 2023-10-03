d15925502453842ce40fadb3f9b920d12998c9d5
NF RIDOY <nfridoy@gmail.com>
2023-10-04 02:54:09 +0600

    Home init

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,6 +2,28 @@
 
 export default function Home() {
     return (
-        <div>Home</div>
+        <div>
+            <main>
+                <section>
+                    <h2>All Caterogy</h2>
+                    <div>
+                        <h2>National News</h2>
+                        <ol>
+                            {
+                                
+                            }
+                        </ol>
+                    </div>
+                </section>
+                <section>
+                    <h2>All Caterogy</h2>
+                </section>
+                <section>
+                    <div>
+                        <h2>Login With</h2>
+                    </div>
+                </section>
+            </main>
+        </div>
     )
 }
